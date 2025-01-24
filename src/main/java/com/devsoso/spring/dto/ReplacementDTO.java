e6e6c8a7c4fd95f6e223989c6b922d04f8d47f11
Soso <sunnzoca@gmail.com>
2025-01-24 15:03:38 -0300

    Games on list moved

---
+++ src/main/java/com/devsoso/spring/dto/ReplacementDTO.java
@@ -0,0 +1,25 @@
+package com.devsoso.spring.dto;
+
+public class ReplacementDTO {
+
+	private Integer sourceIndex;
+	private Integer destionationIndex;
+	
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	
+	public Integer getDestionationIndex() {
+		return destionationIndex;
+	}
+	
+	public void setDestionationIndex(Integer destionationIndex) {
+		this.destionationIndex = destionationIndex;
+	}
+	
+	
+}
